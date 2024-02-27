fe06f28db8db1845422c14ce21896977b5c0107d
egep4 <egep41414141@gmail.com>
2024-02-26 19:24:04 -0800

    current code

---
+++ src/main/java/net/geppp41/tutorialmod/TutorialModClient.java
@@ -0,0 +1,9 @@
+package net.geppp41.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
